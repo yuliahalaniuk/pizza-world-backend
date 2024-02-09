52d4998761f99463fe65388819ade3e750e2cb0e
yuliahalaniuk <122388855+yuliahalaniuk@users.noreply.github.com>
2024-02-09 11:16:19 +0200

    new fix

--- controllers/pizzas-controller.js
+++ controllers/pizzas-controller.js
@@ -1,7 +1,7 @@
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import Pizza from "../models/pizza.js";
 
-async function getAll(req, res, next) {
+async function getAll(_, res) {
   const pizzas = await Pizza.find();
   res.json(pizzas);
 }
