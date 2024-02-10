969efb01c794efd8be610f864cff8dfa8d90712f
yuliahalaniuk <122388855+yuliahalaniuk@users.noreply.github.com>
2024-02-10 22:29:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import pizzaRouter from "./routes/api/pizzasRouter.js";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/pizzas", pizzaRouter);
 
