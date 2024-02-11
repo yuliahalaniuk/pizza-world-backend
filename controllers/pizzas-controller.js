c7fd26168d08a74ad844ae5369ff86f23138a883
yuliahalaniuk <122388855+yuliahalaniuk@users.noreply.github.com>
2024-02-11 22:34:24 +0200

    Update pizzas-controller.js

--- controllers/pizzas-controller.js
+++ controllers/pizzas-controller.js
@@ -2,13 +2,15 @@ import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import Pizza from "../models/pizza.js";
 
 async function getAll(req, res) {
+  if (req.query.category === "all") {
+    const pizzas = await Pizza.find();
+    res.json(pizzas);
+  }
+
   if (req.query.category) {
     const pizzas = await Pizza.find({ category: req.query.category });
     res.json(pizzas);
   }
-
-  const pizzas = await Pizza.find();
-  res.json(pizzas);
 }
 
 export default {
